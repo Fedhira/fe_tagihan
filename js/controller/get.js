87f724f8e536a9ccb5a5357cd943fc3165d10789
Fedhira Syaila <pedirasyaila@gmail.com>
2023-04-07 22:36:44 +0700

    fe_tagihan

--- js/controller/get.js
+++ js/controller/get.js
@@ -8,17 +8,17 @@ export function isiTablePresensi(results) {
 function isiRow(value) {
   let content = isiTabel
     .replace("#NAMA_BANK#", value.nama_bank)
+    .replace("#LOKASI_BANK#", value.lokasi)
     .replace("#NAMA_NASABAH#", value.daftar.total_tagihan.biodata.nama_nasabah)
+    .replace("#PHONE_NUMBER#", value.daftar.total_tagihan.biodata.phone_number)
+    .replace("#EMAILN#", value.daftar.total_tagihan.biodata.email)
     .replace("#TAGIHAN#", value.daftar.total_tagihan.total_tagihan)
+    .replace("#TANGGAL#", value.daftar.total_tagihan.tanggal_jatuhtempo)
+    .replace("#DESKRIPSI#", value.daftar.total_tagihan.deskripsi)
+    .replace("#STATUS#", value.daftar.total_tagihan.status)
     .replace("#NAMA_PENAGIH#", value.daftar.nama_penagih)
-    // .replace("#NOHP#", value.absensi.biodata ? value.absensi.biodata.phonenumber : "#NOHP#")
-    // .replace("#MATAKULIAH#", value.kategori.nama_mk)
-    // .replace("#DOSEN#", value.kategori.pengampu ? value.kategori.pengampu.namadosen : "#DOSEN#")
-    // .replace("#NOHPD#", value.kategori.pengampu.phonenumberd)
-    // .replace("#JAMMASUK#", value.kategori.jadwal ? value.kategori.jadwal.jammasuk : "#JAMMASUK#")
-    // .replace("#JAMKELUAR#", value.kategori.jadwal.jamkeluar)
-    // .replace("#HARI#", value.kategori.jadwal.hari)
-    // .replace("#HADIR#", value.absensi.jumlahkehadiran)
+    .replace("#NO_HP#", value.daftar.phone_number)
+    .replace("#EMAILP#", value.daftar.email)
     .replace("#WARNA#", getRandomColor())
     .replace(/#WARNALOGO#/g, getRandomColorName());
   addInner("iniTabel", content);
